e9b8a58c50b73542824ee5b689ca07a215341b0e
ReaperDevCollins <107894760+ReaperDevCollins@users.noreply.github.com>
2026-09-11 15:08:25 -0700

    Add authentication options to Supabase client

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,4 +3,10 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = 'https://peanoxouvthzdcklgnjl.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlYW5veG91dnRoemRja2xnbmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1NDc0MDEsImV4cCI6MjEwNDEyMzQwMX0.uk3vEUp1OT_Wp-0VyYy6KN5XzPUDtt6LgeSOCOR6vg4';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  });
